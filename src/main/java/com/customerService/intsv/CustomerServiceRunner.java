8182759b68495a989d31156756416a0299c960f8
Igor <Sketch_90@mail.ru>
2023-03-06 18:02:54 +0300

    repare conflicts with master

--- src/main/java/com/customerService/intsv/CustomerServiceRunner.java
+++ src/main/java/com/customerService/intsv/CustomerServiceRunner.java
@@ -1,9 +1,15 @@
 package com.customerService.intsv;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+        @Server( url = "https://customer-registration-service-production.up.railway.app", description = "web"),
+        @Server(url = "http://localhost:8080")
+})
 public class CustomerServiceRunner {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceRunner.class, args);
--- src/main/java/com/customerService/intsv/config/ApplicationConfig.java
+++ src/main/java/com/customerService/intsv/config/ApplicationConfig.java
@@ -1,6 +1,12 @@
 package com.customerService.intsv.config;
 
 import com.customerService.intsv.repository.UserRepository;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +18,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -41,6 +50,29 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("https://customer-registration-service-production.up.railway.app"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("SpringShop API").version("V0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/v3/api-docs", config);
+        return new CorsFilter(source);
     }
 
 }
